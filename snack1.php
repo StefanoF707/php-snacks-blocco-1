dce7d4f639caf0fdcf4b2fc388f753cf033ab848
Stefano Franchini <stefano.franchini77@gmail.com>
2021-01-20 15:12:41 +0100

    snack #1

--- snack1.php
+++ snack1.php
@@ -12,5 +12,74 @@ schema:
 Olimpia Milano - Cantù | 55 - 60
 */
 
-
+    $matches = [
+        [
+            'homeTeam' => 'Denver Nuggets',
+            'awayTeam' => 'Oklahoma City Thunder',
+            'homePoints' => 119,
+            'awayPoints' => 101,
+        ],
+        [
+            'homeTeam' => 'Utah Jazz',
+            'awayTeam' => 'New Orleans Pelicans',
+            'homePoints' => 118,
+            'awayPoints' => 102,
+        ],
+        [
+            'homeTeam' => 'Los Angeles Lakers',
+            'awayTeam' => 'Golden State Warriors',
+            'homePoints' => 113,
+            'awayPoints' => 115,
+        ],
+        [
+            'homeTeam' => 'Chicago Bulls',
+            'awayTeam' => 'Houston Rockets',
+            'homePoints' => 125,
+            'awayPoints' => 120,
+        ],
+        [
+            'homeTeam' => 'Miami Heat',
+            'awayTeam' => 'Detroit Pistons',
+            'homePoints' => 113,
+            'awayPoints' => 117,
+        ],
+        [
+            'homeTeam' => 'Brooklyn Nets',
+            'awayTeam' => 'Milwaukee Buks',
+            'homePoints' => 125,
+            'awayPoints' => 123,
+        ],
+        [
+            'homeTeam' => 'Toronto Raptors',
+            'awayTeam' => 'Dallas Mavericks',
+            'homePoints' => 116,
+            'awayPoints' => 93,
+        ],
+        [
+            'homeTeam' => 'New York Knicks',
+            'awayTeam' => 'Orlando Magic',
+            'homePoints' => 91,
+            'awayPoints' => 84,
+        ],
+    ];
  ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Snack #1</title>
+</head>
+<body>
+
+    <ol>
+        <?php
+            for ($i = 0; $i < count($matches); $i++) {
+                echo "<li>" . $matches[$i]['homeTeam'] . " - " . $matches[$i]['awayTeam'] . " | " . $matches[$i]['homePoints'] . " - " . $matches[$i]['awayPoints'] . "</li>";
+            }
+         ?>
+    </ol>
+
+</body>
+</html>
--- snack2.php
+++ snack2.php
@@ -11,3 +11,15 @@
     “Accesso negato”.
     */
  ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Snack #2</title>
+</head>
+<body>
+
+</body>
+</html>
